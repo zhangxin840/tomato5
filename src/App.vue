95ce6b2e6f1c5c1805b219c3f4acf8dc7dfae74b
XinZhang <zhangxin19@meituan.com>
2016-07-09 16:28:07 +0800

    Basic site style.

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,54 @@
 <template>
   <div id="app">
-    <img class="logo" src="./assets/logo.png">
-    <hello></hello>
-    <p>
-      Welcome to your Vue.js app!
-    </p>
-    <p>
-      To get a better understanding of how this boilerplate works, check out
-      <a href="http://vuejs-templates.github.io/webpack" target="_blank">its documentation</a>.
-      It is also recommended to go through the docs for
-      <a href="http://webpack.github.io/" target="_blank">Webpack</a> and
-      <a href="http://vuejs.github.io/vue-loader/" target="_blank">vue-loader</a>.
-      If you have any issues with the setup, please file an issue at this boilerplate's
-      <a href="https://github.com/vuejs-templates/webpack" target="_blank">repository</a>.
-    </p>
-    <p>
-      You may also want to checkout
-      <a href="https://github.com/vuejs/vue-router/" target="_blank">vue-router</a> for routing and
-      <a href="https://github.com/vuejs/vuex/" target="_blank">vuex</a> for state management.
-    </p>
+    <div class="title">
+      <img class="logo" src="./assets/tomato.svg">
+      <h1>We Tomatoes!</h1>
+    </div>
+
+    <panel></panel>
   </div>
 </template>
 
 <script>
+import normalize from 'normalize.css'; /* eslint no-unused-vars: 0 */
 import Hello from './components/Hello';
+import Panel from './components/Panel';
 
 export default {
   components: {
-    Hello,
+    Hello, Panel,
   },
 };
 </script>
 
 <style>
-html {
+body {
+  display: block;
   height: 100%;
+  font-family: 'Architects Daughter', cursive;
 }
 
-body {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 100%;
+.title{
+  line-height: 100px;
+  text-align: center;
+  margin: 10% 0;
+}
+.title h1 {
+  color: #42b983;
+  text-align: center;
+  font-size: 40px;
+  display: inline-block;
+  margin: 0;
+}
+.title .logo {
+  width: 100px;
+  height: 100px;
+  display: inline-block;
+  vertical-align: top;
 }
 
 #app {
   color: #2c3e50;
-  margin-top: -100px;
-  max-width: 600px;
-  font-family: Source Sans Pro, Helvetica, sans-serif;
-  text-align: center;
 }
 
 #app a {
@@ -57,8 +56,11 @@ body {
   text-decoration: none;
 }
 
-.logo {
-  width: 100px;
-  height: 100px
+.icon-tomato{
+  display: inline-block;
+  width: 30px;
+  height: 30px;
+  text-indent: -9999px;
+  background: url('./assets/tomato.svg');
 }
 </style>
---
+++ src/components/Panel.vue
@@ -0,0 +1,72 @@
+<template>
+  <section class="panel">
+    <div class="user">
+      <div class="avator">
+        <img class="pic" :src="photoURL" alt="avator" />
+        <span class="timer">25:00</span>
+      </div>
+    </div>
+    <div class="task">
+      <div class="list">
+        <span class="icon-tomato">tomaro1</span>
+        <span class="icon-tomato">tomaro1</span>
+        <span class="icon-tomato">tomaro1</span>
+        <span class="icon-tomato">tomaro1</span>
+        <span class="icon-tomato">tomaro1</span>
+        <span class="icon-tomato">tomaro1</span>
+        <span class="icon-tomato">tomaro1</span>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      // note: changing this line won't causes changes
+      // with hot-reload because the reloaded component
+      // preserves its current state and we are modifying
+      // its initial state.
+      name: 'zhangxin840',
+      // photoURL: './assets/tomato.svg',
+      photoURL: 'https:///www.granneman.com/files/cache/716011d79afdd5969b0656b7ad5812b2.jpg',
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  color: #42b983;
+}
+
+.panel{
+  display: flex;
+  width: 80%;
+  max-width: 400px;
+  margin: auto;
+}
+
+.panel .user{
+  margin-right: 30px;
+}
+
+.panel .avator .timer{
+  display: block;
+  text-align: center;
+}
+
+.panel .avator .pic{
+  display: inline-block;
+  width: 50px;
+  height: 50px;
+  border-radius: 25px;
+}
+.panel .list{
+  text-align: left;
+  line-height: 40px;
+}
+
+</style>
